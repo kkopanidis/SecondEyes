a5c904558c9d0ddb56f01b34b13fd88a8d37d920
kkopanidis <kkopanidis@gmail.com>
2015-12-14 23:12:57 +0200

    Calls Working

--- app/src/main/java/com/asoee/smartdrive/Call.java
+++ app/src/main/java/com/asoee/smartdrive/Call.java
@@ -1,6 +1,11 @@
 package com.asoee.smartdrive;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+import java.util.HashMap;
 
 public class Call extends Action {
 
@@ -14,7 +19,10 @@ public class Call extends Action {
 
     @Override
     protected void analyzeSentence() {
-
+        sentence = sentence.toLowerCase();
+        String[] tokens = sentence.split("\\s");
+        if(tokens[0].equals("call"))
+            getContacts(tokens[1]);
     }
 
     @Override
@@ -22,4 +30,19 @@ public class Call extends Action {
         return null;
     }
 
+    void getContacts(String contact) {;
+        Cursor contacts = MainWindow.active_context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
+        while (contacts.moveToNext()) {
+            String name = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)).toLowerCase();
+            String phoneNumber = contacts.getString(contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            if(name.equalsIgnoreCase(contact)){
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                MainWindow.active_context.startActivity(callIntent);
+                return;
+            }
+        }
+
+        contacts.close();
+    }
 }
