b3551ad2f36fee5bc5490a91699a6cda00cb267d
kkopanidis <kkopanidis@gmail.com>
2015-12-15 00:00:58 +0200

    Alarm working

--- app/src/main/java/com/asoee/smartdrive/Alarm.java
+++ app/src/main/java/com/asoee/smartdrive/Alarm.java
@@ -1,6 +1,8 @@
 package com.asoee.smartdrive;
 
+import android.app.AlarmManager;
 import android.content.Intent;
+import android.provider.AlarmClock;
 
 public class Alarm extends Action {
 
@@ -14,6 +16,18 @@ public class Alarm extends Action {
 
     @Override
     protected void analyzeSentence() {
+        sentence = sentence.toLowerCase();
+        String[] tokens = sentence.split("\\s");
+        if(tokens[0].equals("alarm"))
+            if(tokens[1].equals("for"))
+                setAlarm(tokens[2]);
+            else
+                setAlarm(tokens[1]);
+        else if(tokens[0].equals("set") && tokens[1].equals("alarm"))
+            if(tokens[2].equals("for"))
+                setAlarm(tokens[3]);
+            else
+                setAlarm(tokens[2]);
 
     }
 
@@ -21,6 +35,17 @@ public class Alarm extends Action {
     public Intent executeCommandIntent() {
 
         return null;
+    }
+
+    void setAlarm(String time){
+        String hour = time.split(":")[0];
+        String minutes = time.split(":")[1];
+
+        Intent alarm = new Intent(AlarmClock.ACTION_SET_ALARM);
+        alarm.putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(hour));
+        alarm.putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(minutes));
+        alarm.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        MainWindow.active_context.startActivity(alarm);
     }
 
 }
