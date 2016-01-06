a7a25502dcd73848e982eed044ff57fbd4dd4783
kkopanidis <kkopanidis@gmail.com>
2016-01-06 15:02:58 +0200

    fixes, adds etc

--- app/src/main/java/com/asoee/smartdrive/Contact.java
+++ app/src/main/java/com/asoee/smartdrive/Contact.java
@@ -1,12 +1,14 @@
 package com.asoee.smartdrive;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.ContactsContract;
 
 public class Contact extends Action {
-    String name;
-    String number;
-    public Contact(String sentence){
+    String name = " ";
+    String number = " ";
+
+    public Contact(String sentence) {
         super(sentence);
     }
 
@@ -15,29 +17,31 @@ public class Contact extends Action {
         int index = sentence.indexOf("contact");
         String details = sentence.substring(index + "contact".length()).trim();
         String[] tokens = details.split(" ");
-        for (String token : tokens){
+        name = " ";
+        number = " ";
+        for (String token : tokens) {
             if (token.equals(""))
                 continue;
-            if(!token.matches("[0-9]+"))
-                this.name+=token;
+            if (!token.matches("[0-9]+"))
+                this.name += token;
             else {
                 this.number = token;
                 break;
             }
         }
         MainWindow.activity
-                .approveAction("I will add a new contact with name "+this.name+" ,and number "
-                        +this.number+" .Is that correct?",true);
+                .approveAction("I will add a new contact with name " + this.name + " ,and number "
+                        + this.number + " .Is that correct?", true);
     }
 
     @Override
     public void executeCommand() {
-        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
-            // Sets the MIME type to match the Contacts Provider
+        Intent intent = new Intent( ContactsContract.Intents.Insert.ACTION);
+        // Sets the MIME type to match the Contacts Provider
         intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
         intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
         intent.putExtra(ContactsContract.Intents.Insert.PHONE, number);
-        intent.putExtra(ContactsContract.Intents.EXTRA_FORCE_CREATE, false );
+        intent.putExtra(ContactsContract.Intents.EXTRA_FORCE_CREATE, true);
         MainWindow.activity.startActivity(intent);
     }
 }
